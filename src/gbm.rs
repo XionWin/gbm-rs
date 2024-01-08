f9dc32943c9ab730b22e71f1132f87671d8ba02e
XionWin <xionwin@hotmail.com>
2024-01-08 21:11:56 +0800

    Update

--- src/gbm.rs
+++ src/gbm.rs
@@ -1,11 +1,8 @@
-use std::os::unix::prelude::RawFd;
-
 use crate::{Device, Surface, def::{SurfaceFormat, FormatModifier}};
 
 #[derive(Debug)]
 pub struct Gbm {
     pub drm: drm_rs::core::Drm,
-    pub fd: RawFd,
     pub surface: Surface,
     pub surface_format: SurfaceFormat,
     pub format_modifiers: Vec<FormatModifier>,
@@ -16,14 +13,11 @@ pub struct Gbm {
 impl Gbm {
     pub fn new(drm: drm_rs::core::Drm, surface_format: SurfaceFormat, format_modifiers: Vec<FormatModifier>) -> Self
     {
-        let fd = drm.fd();
-        let crtc = drm.get_crtc();
-        let width = crtc.get_width();
-        let height = crtc.get_height();
-        let surface = Surface::new_with_modifiers(Device::new(fd), width, height, surface_format, &format_modifiers);
+        let width = drm.crtc.get_width();
+        let height = drm.crtc.get_height();
+        let surface = Surface::new_with_modifiers(Device::new(drm.get_fd()), width, height, surface_format, &format_modifiers);
         Self{
             drm,
-            fd,
             surface,
             surface_format,
             format_modifiers,
